14df726ae41e234e92bda2e5e237d09566aff2ad
Ana Tran <ana.tran323@gmail.com>
2023-02-23 19:41:01 -0600

    createInitalUsers done, working on createInitialMakes1

--- db/seedData.js
+++ db/seedData.js
@@ -39,7 +39,7 @@ async function createTables() {
       "typeId" INTEGER REFERENCES type(id),
       "makeId" INTEGER REFERENCES make(id),
       "usersId" INTEGER REFERENCES users(id),
-      color TEXT,
+      color VARCHAR(255),
       mileage INTEGER,
       UNIQUE ("typeId", "makeId", "userId") 
     );
@@ -81,9 +81,10 @@ async function createTables() {
     CREATE TABLE photos (
       id SERIAL PRIMARY KEY, 
       "carsId" INTEGER REFERENCES cars(id),
-      image VARBINARY(MAX)
+      image TEXT
     );
-    `); //NOTE SHANNON WHAT IS 255>???
+    `); //use the string of the text to the url link to image
+    //could download image to then add to source file on here
     console.log('finished creating tables!');
   } catch (error) {
     console.error('Error in creating tables...');
@@ -91,13 +92,31 @@ async function createTables() {
   }
 }
 
-async function populateInitialData() {
+async function createInitialUsers() {
   try {
-    // create useful starting data by leveraging your
-    // Model.method() adapters to seed your db, for example:
-    // const user1 = await User.createUser({ ...user info goes here... })
+    console.log("Starting to create users...");
+    const usersToCreate = [
+      { username: 'bobby', password: 'bobby123', isAdmin: true},
+      { username: 'ana', password: 'ana123456', isAdmin: false },
+      { username: 'zack', password: 'zack123456', isAdmin: true }
+    ];
+    const users = await Promise.all(usersToCreate.map(createUser)); //don't forget to write this function "createUser"
+
+    console.log('Users created:');
+    console.log(users);
+    console.log('Finished creating users!');
   } catch (error) {
+    console.error("Error creating users...");
     throw error;
+  }
+}
+
+async function createInitialMakes() {
+  try {
+    console.log("Starting to create makes");
+    const makesToCreate = 
+  } catch (error) {
+    
   }
 }
 
