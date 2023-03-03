7fb790cd2c2d5230cb6190fcd233ca555dde23f5
Ana Tran <ana.tran323@gmail.com>
2023-03-02 21:15:18 -0600

    yeah im back :D

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,4 +3,6 @@ export { default as Cars } from './Cars';
 export { default as Cart } from './Cart';
 export { default as Home } from './Home';
 export { default as Login } from './Login';
+export { default as Navbar } from './Navbar';
 export { default as Register } from './Register';
+export { default as NotFound } from './NotFound'
