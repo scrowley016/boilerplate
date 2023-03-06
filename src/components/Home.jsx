ad78196bb60e237d358ab3a5d400d92ab45583a3
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2023-03-05 21:40:59 -0500

    updates to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
+import { CommandCompleteMessage } from 'pg-protocol/dist/messages';
 import React, { useState, useEffect } from 'react';
 import { fetchAllCars, fetchAllTypes, fetchAllModels, fetchAllMakes } from '../api/index';
-// import { getMakeByName } from '../db/make';
+import { BrowserRouter, Link, Route, Switch, useHistory } from 'react-router-dom';
+import { Cars } from './index';
 
 const Home = () => {
   const [cars, setCars] = useState([]);
@@ -8,71 +10,77 @@ const Home = () => {
   const [models, setModels] = useState([]);
   const [makes, setMakes] = useState([]);
 
-  const [selectedmake, setSelectedmake] = useState('');
-  const [selectedmodel, setSelectedmodel] = useState('');
+  const [selectedmakeId, setSelectedmakeId] = useState('');
+  const [selectedmodelId, setSelectedmodelId] = useState('');
   const [selectedprice, setSelectedprice] = useState('');
 
   const [carsearch, setCarsearch] = useState([])
 
-  const handleModel = (event) => {
-    setSelectedmodel(event.target.value)
+  const loadCars = async () => {
+    const cars = await fetchAllCars();
+    const types = await fetchAllTypes();
+    const makes = await fetchAllMakes();
+    const models = await fetchAllModels();
+    setCars(cars);
+    setTypes(types)
+    setMakes(makes);
+    setModels(models);
   }
 
-  const handlePrice = (event) => {
-    setSelectedprice(event.target.value)
-  }
-
-  console.log("make:", selectedmake)
-  console.log("model:", selectedmodel)
+  console.log(carsearch)
 
   useEffect(() => {
-    const homePage = async () => {
-      const cars = await fetchAllCars();
-      const types = await fetchAllTypes();
-      const makes = await fetchAllMakes();
-      const models = await fetchAllModels();
-      setCars(cars);
-      setTypes(types)
-      setMakes(makes);
-      setModels(models);
-    };
-    homePage();
+    loadCars();
   }, []);
 
-// console.log("price:", selectedprice)
-
   return (
+    <BrowserRouter>
+      <div className='app-container'>
 
-    <div className='app-container'>
+        <h1>Begin your adventure...</h1>
+        <form id='searchBar'>
+          <select className='select' value={selectedmakeId} onChange={(event) => { setSelectedmakeId(event.target.value) }}>
+            <option value=''>Make</option>
+            {makes.map((e, i) => (<option key={i} value={e.id}>{e.name}</option>))}
+          </select>
+          <select className='select' value={selectedmodelId} onChange={(event) => { setSelectedmodelId(event.target.value) }}>
+            <option value=''>Model</option>
+            {models.filter((model) => model.makeId == selectedmakeId).map((e, j) => (<option key={j} value={e.id}>{e.name}</option>))}
+          </select>
+          <select className='select' value={selectedprice} onChange={(event) => { setSelectedprice(event.target.value) }}>
+            <option value='0'>Max Price</option>
+            <option>$20,000</option>
+            <option>$40,000</option>
+            <option>$60,000</option>
+            <option>$80,000</option>
+            <option>$100,000</option>
+            <option>$120,000</option>
+            <option>$140,000</option>
+            <option>$160,000</option>
+            <option>$180,000</option>
+            <option>$200,000</option>
+          </select>
 
 
-      <h1>Home Page</h1>
-      <form id='searchBar'>
-        <select value={selectedmake} onChange={(event) => {setSelectedmake(event.target.value)}}>
-          <option value=''>Make</option>
-          {makes.map((e, i) => (<option key={i} value={e.id}>{e.name}</option>))}
-        </select>
-        <select value={selectedmodel} onChange={handleModel}>
-          <option value=''>Model</option>
-          {models.filter((model) => model.makeId == selectedmake).map((e, j) => (<option key={j} value={e.id}>{e.name}</option>))}
-        </select>
-        <select value={selectedprice} onChange={handlePrice}>
-          <option value='0'>Max Price</option>
-          <option>$20,000</option>
-          <option>$40,000</option>
-          <option>$60,000</option>
-          <option>$80,000</option>
-          <option>$100,000</option>
-          <option>$120,000</option>
-          <option>$140,000</option>
-          <option>$160,000</option>
-          <option>$180,000</option>
-          <option>$200,000</option>
-        </select>
-        <button id='searchButton'>Search</button>
-      </form>
+          <Link to="/Cars">
+            <div className='button' onClick={(event) => {
+              setCarsearch([selectedmakeId, selectedmodelId, selectedprice]);
+            }}>
+              Search
+            </div>
+          </Link>
 
-    </div>
+        </form>
+
+        <Switch>
+          <Route path="/Cars">
+            <Cars carsearch={carsearch}/>
+          </Route>
+        </Switch>
+
+
+      </div>
+    </BrowserRouter>
   );
 };
 
